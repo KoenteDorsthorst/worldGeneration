f319c44982b8ee98551258c7c933d8c194dec86d
Koen te Dorsthorst <KoenteDorsthorst@gmail.com>
2021-12-08 00:30:58 +0100

    Made setup
    You have a character that can move,
    You have a little randomized world to walk on,
    What more do you want?

---
+++ worldGeneration/Assets/Scripts/Generation.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generation : MonoBehaviour
+{
+    GameObject objToSpawn;
+
+
+    int xStartPoint = -20;
+    int yStartPoint = -20;
+
+    int width = 40;
+    int height = 40;
+
+    void Start()
+    {
+        
+        
+
+        for(int i = xStartPoint; i < width; i++)
+        {
+            for(int n = yStartPoint; n < height; n++)
+            {
+                GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+
+
+                int random = Random.Range(0, 2);
+                cube.transform.position = new Vector3(i, random, n);
+                cube.AddComponent<BoxCollider>();
+                cube.name = "Cube " + i + " " + n;
+            }
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ worldGeneration/Assets/Scripts/Movement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    // float speed = 0.2F;
+
+    int movementSpeed = 5;
+
+    private void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+
+
+        float x = 5 * Input.GetAxis("Mouse X");
+        transform.Rotate(0, x, 0);
+
+
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+
+        //Vector3 movement = new Vector3(h, 0, v) * speed;
+        //transform.position += movement.x * Vector3.forward;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += transform.forward * Time.deltaTime * movementSpeed;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            transform.position -= transform.forward * Time.deltaTime * movementSpeed;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            transform.position -= transform.right * Time.deltaTime * movementSpeed;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += transform.right * Time.deltaTime * movementSpeed;
+        }
+
+
+    }
+}
