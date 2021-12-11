e686bab66fb190b580f950f8b17fc328a3c96919
Koen te Dorsthorst <KoenteDorsthorst@gmail.com>
2021-12-11 01:09:10 +0100

    Refactored code a little
    This is all building up to something really cool

--- worldGeneration/Assets/Scripts/Generation.cs
+++ worldGeneration/Assets/Scripts/Generation.cs
@@ -7,47 +7,60 @@ public class Generation : MonoBehaviour
     GameObject objToSpawn;
 
 
-    int xStartPoint = -20;
-    int yStartPoint = -20;
+    int xStartPoint = 0;
+    int yStartPoint = 0;
 
-    int width = 40;
-    int height = 40;
+    int width = 10;
+    int length = 10;
 
     void Start()
     {
+
+
+        int[,] heightMap = new int[length, width];
+
+        heightMap = fillInHeightMap(heightMap);
+
+        heightMap[3, 7] = 3;
+
+        
         
         
 
-        for(int i = xStartPoint; i < width; i++)
+        for(int i = yStartPoint; i < length; i++)
         {
-            for(int n = yStartPoint; n < height; n++)
+            Debug.Log(i);
+            for(int n = xStartPoint; n < width; n++)
             {
-                
-
-
-                
-                int random = Random.Range(1, 10);
-                int height = 1;
-                if(random > 8)
-                {
-                    height = 3;
-                }
-                else if(random > 7){
-                    height = 2;
-                }
+                Debug.Log("------");
+                Debug.Log(n);
+                int height = heightMap[i, n];
 
 
                 for (int h = 0; h < height; h++)
                 {
                     GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-                    cube.transform.position = new Vector3(i, h, n);
+                    cube.transform.position = new Vector3(n, h, i);
                     cube.AddComponent<BoxCollider>();
                     cube.name = "Cube " + i + " " + n + " " + h;
                 }
-                
+                Debug.Log(i);
+                Debug.Log(height);
             }
         }
         
+    }
+
+    int[,] fillInHeightMap(int[,] array)
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int n = 0; n < array.GetLength(1); n++)
+            {
+                array[i, n] = 1;
+            }
+        }
+        return array;
     }
 
     // Update is called once per frame
